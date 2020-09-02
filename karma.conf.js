b3cb22058e665f2d1e56fe7873543f94e9444562
Artur Kudlacz <akudlacz@adobe.com>
2020-09-02 15:20:31 +0200

    ci(semantic-release): update code coverage step

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
+      dir: require('path').join(__dirname, 'coverage'),
       reports: ['text', 'html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
