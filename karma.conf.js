c09e16d7d14ddceb333eb4495b7129365553d117
Artur Kudlacz <akudlacz@adobe.com>
2020-09-02 14:44:17 +0200

    ci(semantic-release): update karma config
    - use headless chrome for testing

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,13 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: true
+    browsers: ['ChromeCI'],
+    singleRun: true,
+    customLaunchers: {
+      ChromeCI: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--headless' ]
+      }
+    }
   });
 };
