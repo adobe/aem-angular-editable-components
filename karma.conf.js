82695e443a6f7a03369900324c3af2600f6b7b95
Niek Raaijmakers <niekraaijmakers@gmail.com>
2021-01-11 19:03:32 +0100

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function(config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: [ 'ChromeCI' ],
-    singleRun: false,
+    singleRun: true,
     customLaunchers
   });
 };
